e7137e5165dc76dc9cae76a7906872a6bd1d8935
Travis Draper <tdanieldraper@gmail.com>
2021-05-26 15:32:18 -0400

    .

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
